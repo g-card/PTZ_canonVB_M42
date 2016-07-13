8810ead5f5123d89ce8c912a4f18bd60755b1714
g-card <gregoire47@gmail.com>
2016-07-13 15:49:31 +0200

    Separated file for command.

---
+++ ros_pkg/src/node/cmd.cpp
@@ -0,0 +1,178 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+#include <dynamic_reconfigure/server.h>
+#include <canon_vbm42/CanonParamsConfig.h>
+#include <canon_vbm42/PTZ.h>
+#include <string>
+#include <vector>
+#include "libCanon/CanonDriver.h"
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include "capteurs.h"
+#include "homog.cpp"
+
+using namespace std;
+using namespace boost;
+using namespace std;
+using namespace cv;
+
+Mat img_out,H,obj;
+canon_vbm42::PTZ ptz;
+Mat frame;
+int flag=0;
+int homog=0;
+vector<int> nb_pieces; //orange,jaune
+vector<vector<Point2f> > centre_pieces; //orange,jaune
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    switch(homog)
+    {
+      case 1:frame = cv_bridge::toCvCopy(msg, "bgr8")->image;
+	homog_up(1,imread("/home/etudiant/catkin_ws/src/canon_vbm42/src/Poste_1_up.JPG"), frame, &img_out, &H);
+	homog=0;
+	flag=true;
+	break;
+      case 2:frame = cv_bridge::toCvCopy(msg, "bgr8")->image;
+	homog_up(2,imread("/home/etudiant/catkin_ws/src/canon_vbm42/src/Poste_2_up.JPG"), frame, &img_out, &H);
+	homog=0;
+	flag=true;
+	break;
+      case 4:
+	break;
+      case 5:
+	break;
+      default:
+	break;
+    }
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+
+void point(int choix, float *p, float *t, float *z)
+{
+  FILE *cmd_file;
+  int num_poste = 0;
+  float coord[5][3];
+  float pan, tilt, zoom;
+
+  cmd_file = fopen("/home/etudiant/catkin_ws/src/canon_vbm42/src/node/pointage.txt","r");
+  if(cmd_file == NULL)
+  {
+    perror("Error");
+  }
+  for(int i = 0; i < 4; i++)
+  {
+    fscanf(cmd_file,"%d:\n",&num_poste);
+    fscanf(cmd_file,"P:%f\nT:%f\nZ:%f\n",&pan,&tilt,&zoom);
+    coord[num_poste][1] = pan;
+    coord[num_poste][2] = tilt;
+    coord[num_poste][3] = zoom;
+  }
+  *p = coord[choix][1];
+  *t = coord[choix][2];
+  *z = coord[choix][3];
+  if(fclose(cmd_file) == 0)
+    cout << "Param. lu " <<*p<<", "<<*t<<", "<<*z<< endl;
+}
+
+ int main(int argc, char* argv[]) 
+{
+    bool PSx[25],DxD[13],DxG[13],CPx[11],CPIx[9];
+    	for(int i=0;i<25;i++) PSx[i]=0;
+	for(int i=0;i<13;i++) DxD[i]=0;
+	for(int i=0;i<13;i++) DxG[i]=0;
+	for(int i=0;i<11;i++) CPx[i]=0;
+	for(int i=0;i<9;i++) CPIx[i]=0;
+    bool center=0,p2=0,p1=0,p4=0,p5=0; //flag poste ou centre
+    ros::init(argc, argv, "cmd_camera");
+    ros::NodeHandle nh("~");
+    Capteurs Capteurs(nh);
+    ros::Publisher ptzpub = nh.advertise<canon_vbm42::PTZ>("cmd_ptz", 1);
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber sub = it.subscribe("/canon_vbm42/image", 1, imageCallback);
+    //ros::Subscriber ptzsub = nh.subscribe("/canon_vbm42/ptz_pos",1,ptzCallback);
+
+    int capt;
+    center=1;
+    
+    ros::Rate loop_rate(10);
+
+    int tot_pieces;
+    while (ros::ok()) {
+      
+      Capteurs.Actualiser(PSx,DxD,DxG,CPx,CPIx);
+      if(PSx[21]&&(!p2))
+	{
+	  cout<<"Pointage 2"<<endl;
+	  point(2,&ptz.pan,&ptz.tilt,&ptz.zoom);
+	  ptzpub.publish(ptz);sleep(4);
+	  homog=2;
+	  center=0;
+	  p2=1;
+	}else{
+	  if(PSx[3]&&(!p1)){
+	    cout<<"Pointage 1"<<endl;
+	    point(1,&ptz.pan,&ptz.tilt,&ptz.zoom);
+	    ptzpub.publish(ptz);sleep(4);
+	    homog=1;
+	    center=0;
+	    p1=1;
+	  }else{
+	    if(PSx[9]&&(!p4)){
+	      cout<<"Pointage 4"<<endl;
+	      point(4,&ptz.pan,&ptz.tilt,&ptz.zoom);
+	      ptzpub.publish(ptz);
+	      center=0;
+	      p4=1;
+	    }
+	    else{
+	      if(PSx[15]&&(!p5)){
+		cout<<"Pointage 5"<<endl;
+		point(5,&ptz.pan,&ptz.tilt,&ptz.zoom);
+		ptzpub.publish(ptz);
+		center=0;
+		p5=1;
+	      }
+	      if(!center&& ((p5&&PSx[17]) || (p4&&PSx[12]) || (p2&&PSx[24]) || (p1&&PSx[5]) ) ){
+		cout<<"Pointage centre"<<endl;
+		ptz.pan=11;
+		ptz.tilt=-20;
+		ptz.zoom=41;
+		ptzpub.publish(ptz);
+		p5=0;p4=0;p2=0;p1=0;
+		center=1;
+	      }
+	    }
+	  }
+	}
+	if(flag)
+	{
+	  tot_pieces = pieces_sur_nav(/*&centre_pieces[0],&centre_pieces[1],*/img_out/*,&nb_pieces[0],&nb_pieces[1]*/);
+	  flag=0;
+	}
+	
+	  
+	ros::spinOnce();
+	loop_rate.sleep();
+	
+    }
+    
+    printf("\n");
+    return 0;
+}
--- ros_pkg/src/node/node.cpp
+++ ros_pkg/src/node/node.cpp
@@ -19,7 +19,7 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 #include "capteurs.h"
-
+#include "homog.cpp"
 
 using namespace std;
 using namespace boost;
@@ -30,110 +30,31 @@ using namespace cv;
 canon_vbm42::CanonParamsConfig currentConfig;
 boost::recursive_mutex param_mutex;
 CanonDriver * driver = NULL;
-/*
-void callback(canon_vbm42::CanonParamsConfig &config, uint32_t level)
+
+void ptzCallback(canon_vbm42::PTZ ptz)
 {
     cout<<"In callback"<<endl;
-    if (config.night_mode != currentConfig.night_mode) {
-		driver->setNightMode(config.night_mode);
-    }
-    /*if (config.fps != currentConfig.fps) {
-		driver->setFrameRate(config.fps);
-    }
     bool pos_change = false;
-    if (config.pan_ang != currentConfig.pan_ang) {
-        pos_change = true;
+    float current_pan,current_tilt,current_zoom;
+    driver->getCurrentPos(&current_pan,&current_tilt,&current_zoom);
+    if (ptz.pan != current_pan && ptz.tilt != current_tilt && ptz.zoom != current_zoom) {
+
+	  driver->moveTo(ptz.pan,ptz.tilt,ptz.zoom);
     }
-    if (config.tilt_ang != currentConfig.tilt_ang) {
-        pos_change = true;
-    }
-    if (config.zoom_ang != currentConfig.zoom_ang) {
-        pos_change = true;
-    }
-	if (config.pan_speed != currentConfig.pan_speed) 
-		driver->setPanSpeed(currentConfig.pan_ang,config.pan_speed);
-	if (config.tilt_speed != currentConfig.tilt_speed) 
-		driver->setTiltSpeed(currentConfig.tilt_ang,config.tilt_speed);
-	if (config.zoom_speed != currentConfig.zoom_speed) 
-		driver->setZoomSpeed(currentConfig.zoom_ang,config.zoom_speed);
-
-    if (pos_change) {
-        //driver->moveTo(config.pan_ang,config.tilt_ang,config.zoom_ang);
-	cout<<"Moving..."<<endl;
-    }
-#if 0
-
-	// if ((local.width != buffer.width) || 
-	// 		(local.height != buffer.height))
-	// 	driver->setImageSize(local.width,local.height);
-
-	if (config.focus_mode != currentConfig.focus_mode) {
-        switch(config.focus_mode) {
-            case 0:
-                driver->setFocusMode(CanonDriver::FMAuto, CanonDriver::FMUndef);
-                break;
-            case 1:
-                driver->setFocusMode(CanonDriver::FMManual, CanonDriver::FMManualFar);
-                break;
-            case 2:
-                driver->setFocusMode(CanonDriver::FMManual, CanonDriver::FMManualNear);
-                break;
-            case 3:
-                driver->setFocusMode(CanonDriver::FMAutoDomes, CanonDriver::FMUndef);
-                break;
-            case 4:
-                driver->setFocusMode(CanonDriver::FMInfinity, CanonDriver::FMUndef);
-                break;
-        }
-	}
-
-    if (config.autoexp != currentConfig.autoexp) {
-        driver->setAutoExposure("auto");
-        printf("Switched to auto exposure\n");
-	} else if ((config.aperture != currentConfig.aperture) ||
-				(config.inv_shutter != currentConfig.inv_shutter) ||
-				(config.gain != currentConfig.gain)) {
-        driver->setExposureParameters(config.aperture,
-                config.inv_shutter,config.gain);
-        printf("Changed exposure parameters\n");
-	}
-
-	if (config.digital_zoom != currentConfig.digital_zoom) {
-		driver->setDigitalZoom(config.digital_zoom);
-	}
-	if (config.pause != currentConfig.pause) {
-		driver->setVideoReceptionPauseStatus(config.pause);
-	}
-	if (config.record != currentConfig.record) {
-        driver->setRecordingDestination(config.record_dir.c_str());
-		driver->setVideoRecordingMode(config.record);
-		if (config.record) {
-			fprintf(stderr,"Warning: video recording started\n");
-		}
-	}
-    currentConfig = config;
-#endif
-}*/
- main(int argc, char* argv[]) 
+}
+int main(int argc, char* argv[]) 
 {
 	if (argc < 2) {
 		return 1;
 	}
-    bool PSx[25],DxD[13],DxG[13],CPx[11],CPIx[9];
-    	for(int i=0;i<25;i++) PSx[i]=0;
-	for(int i=0;i<13;i++) DxD[i]=0;
-	for(int i=0;i<13;i++) DxG[i]=0;
-	for(int i=0;i<11;i++) CPx[i]=0;
-	for(int i=0;i<9;i++) CPIx[i]=0;
-    bool center=0,p2=0,p1=0,p4=0,p5=0;
+
     currentConfig.hostname = argv[1];
     currentConfig.subsampling = 0;
-    //currentConfig.fps = 10;
-    cout<<get_selfpath()<<endl;
     ros::init(argc, argv, "canon_vbm42");
     ros::NodeHandle nh("~");
     Capteurs Capteurs(nh);
-    ros::Publisher ptzpub = nh.advertise<canon_vbm42::PTZ>("ptz", 1);
+    ros::Subscriber ptzsub=nh.subscribe<canon_vbm42::PTZ>("/cmd_camera/cmd_ptz",1,ptzCallback);
+    ros::Publisher ptzpub = nh.advertise<canon_vbm42::PTZ>("ptz_pos", 1);
     image_transport::ImageTransport it(nh);
     image_transport::Publisher image_pub = it.advertise("image",1);
     int capt;
@@ -143,15 +64,15 @@ void callback(canon_vbm42::CanonParamsConfig &config, uint32_t level)
     if (argc >= 3) {
 	    loginf = argv[2];
     }
-    double p=11.08,t=-20.07,z=41;
+    float p=11,t=-20,z=41;
     float valp,valt,valz;
     /**** Initialising Canon Driver *****/
     CanonDriver canon(currentConfig.hostname.c_str());
     driver = &canon;
-	 if (!canon.connect()) {
+	 if (!driver->connect()) {
 		    return 1;
 	    }
-    driver->moveTo(16.6,-27.49,60.29);//centering
+    driver->moveTo(p,t,z);//centering
     cout<<"Init done"<<endl;
     const std::string videoStreamAddress = "http://"+lexical_cast <string>(argv[1])+"/-wvhttp-01-/video.cgi?.mjpg"; 
     cv::VideoCapture cap(videoStreamAddress);
@@ -163,11 +84,11 @@ void callback(canon_vbm42::CanonParamsConfig &config, uint32_t level)
     cout<<"Video stream opened"<<endl;
     Mat frame;
     sensor_msgs::ImagePtr msg;
-    canon.getCurrentPos(&p,&t,&z);
+    driver->getCurrentPos(&p,&t,&z);
     currentConfig.pan_ang = p;
     currentConfig.tilt_ang = t;
     currentConfig.zoom_ang = z;
-    ros::Rate loop_rate(5);
+    ros::Rate loop_rate(20);
     //dynamic_reconfigure::Server<canon_vbm42::CanonParamsConfig> srv(param_mutex,nh);
     //dynamic_reconfigure::Server<canon_vbm42::CanonParamsConfig>::CallbackType f = boost::bind(&callback, _1, _2);
     //srv.setCallback(f);
@@ -177,50 +98,14 @@ void callback(canon_vbm42::CanonParamsConfig &config, uint32_t level)
     while (ros::ok()) {
 	    
 	canon_vbm42::PTZ ptz;
-	canon.getCurrentPos(&p,&t,&z);
-	cout<<p<<","<<t<<","<<z<<endl;
+	driver->getCurrentPos(&p,&t,&z);
+	//cout<<p<<","<<t<<","<<z<<endl;
 	ptz.pan = p;
 	ptz.tilt = t;
 	ptz.zoom = z;
 	ptzpub.publish(ptz);
-	Capteurs.Actualiser(PSx,DxD,DxG,CPx,CPIx);
-	cap.read(frame);
-	if(PSx[22])
-	{
-	  driver->point(2,&valp,&valt,&valz);cout<<"Pointing to 2..."<<endl;
-	  driver->moveTo(valp,valt,valz);
-	  center=0;p2=1;
-	}
-	else{
-	  if(PSx[2]){
-	    driver->point(1,&valp,&valt,&valz);cout<<"Pointing to 1..."<<endl;
-	    driver->moveTo(valp,valt,valz);
-	    center=0;p1=1;
-	  }
-	  else{
-	    if(PSx[9]){
-	      driver->point(4,&valp,&valt,&valz);cout<<"Pointing to 4..."<<endl;
-	      driver->moveTo(valp,valt,valz);
-	      center=0;p4=1;
-	    }
-	    else{
-	      if(PSx[15]){
-		driver->point(5,&valp,&valt,&valz);cout<<"Pointing to 5..."<<endl;
-		driver->moveTo(valp,valt,valz);
-		center=0;p5=1;
-	      }
-	      else
-	      {
-		if(center==0 &&( (PSx[24]&&p2)|| (PSx[5]&&p1)|| (PSx[12]&&p4)|| (PSx[17]&&p5))){
-		  driver->moveTo(16.6,-27.49,60.29);//centering
-		  center=1;
-		}
-	      }
-	    }
-	  }
-	}
+	cap>>frame;
 	if(!frame.empty()){
-	  //cout<<"envoi"<<endl;
 	    msg = cv_bridge::CvImage(std_msgs::Header(),"bgr8",frame).toImageMsg();
 	    image_pub.publish(msg);
 	}
@@ -229,7 +114,6 @@ void callback(canon_vbm42::CanonParamsConfig &config, uint32_t level)
 	
     }
 
-    canon.disconnect();
     driver = NULL;
 
     
